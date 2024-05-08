c98569775f94fd65f6a49de32e6de1342f1862cf
Aman Kumar <78886335+Aman-kumar-1999@users.noreply.github.com>
2024-05-08 17:16:08 +0000

    added client and about

--- vivekananda-it-solution/src/components/HomePage/HomePageClient.jsx
+++ vivekananda-it-solution/src/components/HomePage/HomePageClient.jsx
@@ -1,10 +1,66 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
+export default function () {
 
-export default function() {
+    const responsive = {
+        superLargeDesktop: {
     
-    
+          breakpoint: { max: 4000, min: 1024 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+
+    const brandData = [
+        {
+          id: 1,
+          imageurl: "clients/client-1.png"
+        },
+        {
+          id: 2,
+          imageurl: "clients/client-2.png"
+        },
+        {
+          id: 3,
+          imageurl: "clients/client-3.png"
+        },
+        {
+          id: 4,
+          imageurl: "clients/client-4.png"
+        }
+        , {
+          id: 5,
+          imageurl: "clients/client-5.png"
+        },
+        {
+          id: 6,
+          imageurl: "clients/client-6.png"
+        },
+        {
+          id: 7,
+          imageurl: "clients/client-7.png"
+        },
+        {
+          id: 8,
+          imageurl: "clients/client-8.png"
+        }
+
+      ]
 
     return (
         <>
@@ -14,8 +70,18 @@ export default function() {
                 <div className="container">
 
                     <div className="swiper">
-                     
-                        <div className="swiper-wrapper align-items-center">
+
+                        <Carousel autoPlay responsive={responsive} className='swiper-wrapper align-items-center'>
+                            {
+                                brandData.map(item => (
+                                    <img style={{ width: "200px", height: "50px" }} name={item.name} src={item.imageurl} />
+                                ))
+                            }
+                            
+                          
+                        </Carousel>
+
+                        {/* <div className="swiper-wrapper align-items-center">
                             <div className="swiper-slide"><img src="clients/client-1.png" className="img-fluid" alt="" /></div>
                             <div className="swiper-slide"><img src="clients/client-2.png" className="img-fluid" alt="" /></div>
                             <div className="swiper-slide"><img src="clients/client-3.png" className="img-fluid" alt="" /></div>
@@ -24,8 +90,8 @@ export default function() {
                             <div className="swiper-slide"><img src="clients/client-6.png" className="img-fluid" alt="" /></div>
                             <div className="swiper-slide"><img src="clients/client-7.png" className="img-fluid" alt="" /></div>
                             <div className="swiper-slide"><img src="clients/client-8.png" className="img-fluid" alt="" /></div>
-                        </div>
-                   
+                        </div> */}
+
                     </div>
 
                 </div>
